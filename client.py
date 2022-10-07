5b5f30c41e6c500d9d5214447a59b447910eb1f6
rvb10 <103139864+rvb10@users.noreply.github.com>
2022-10-08 00:41:20 +0530

    Updated functions as per Task-1

--- client.py
+++ client.py
@@ -42,9 +42,7 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    if price_a==0:
-        return
-    elif price_b==0:
+    if price_b==0:
         return
     else:
         return price_a/price_b 
