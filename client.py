7cefe6ec5aa400185831da935e599f4f8d53ae9c
DeazStar <deadfromtheborn@gmail.com>
2022-10-29 11:29:27 -0400

    fix

--- client.py
+++ client.py
@@ -35,14 +35,14 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
-    return stock, bid_price, ask_price, price
+    price = (bid_price + ask_price) / 2
+    return tuple(stock, bid_price, ask_price, price)
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    return price_a % price_b
 
 
 # Main
@@ -54,6 +54,6 @@ if __name__ == "__main__":
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            print("Quoted {} at (bid:{}, ask:{},price:{}").format(stock, bid_price, ask_price, price)
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio {:d}".format(price, price))
