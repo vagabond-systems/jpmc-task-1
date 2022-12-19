060df22ca81cfa74ef91e5fe2cbc6f51b01bf58c
Tanusree <tanusree.p123@gmail.com>
2022-12-19 13:07:37 +0530

    task1

--- client.py
+++ client.py
@@ -43,9 +43,8 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if price_b==0:
-        return
-    else:    
-        return price_a/price_b
+        return   
+    return price_a/price_b
 
 
 # Main
