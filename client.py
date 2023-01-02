1ffc378c9b98d4e4bdcdfb30495023767f40dcdf
Caesar Wong <caesar.wongw@gmail.com>
2023-01-02 14:25:32 -0800

    change return value for the getRatio function

--- client.py
+++ client.py
@@ -42,7 +42,10 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        # avoid throwing ZeroDivisionError when price_b is 0
+        return
+    return price_a / price_b
 
 
 # Main
