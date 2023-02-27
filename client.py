c1672303f47597dc5c5c5ac9f653345c9982fe42
teej27 <oghenetejiriekrokpe@gmail.com>
2023-02-27 17:47:22 +0000

    Task1 commit

--- client.py
+++ client.py
@@ -35,25 +35,35 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price: float = (bid_price + ask_price) / 2  # calculate the average price
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    prices = {}  # dictionary to store the prices of each stock
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # update prices dictionary with latest prices
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price
 
-        print("Ratio %s" % getRatio(price, price))
+        # calculate ratio of two different stocks
+        stock_a, stock_b = random.sample(prices.keys(), 2)
+        price_a, price_b = prices[stock_a], prices[stock_b]
+        ratio = getRatio(price_a, price_b)
+
+        print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock_a, bid_price, ask_price, price_a))
+        print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock_b, bid_price, ask_price, price_b))
+        print("Ratio of %s and %s: %s" % (stock_a, stock_b, ratio))
