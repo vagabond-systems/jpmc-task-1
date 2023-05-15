8575667bec6d892e9be115766d871f9be53d9706
Sravani <120193768+vadalisravani@users.noreply.github.com>
2023-05-15 10:40:51 +0530

    client.py

--- client.py
+++ client.py
@@ -45,20 +45,22 @@ def getRatio(price_a, price_b):
     if(price_b==0):
         return
     
-    return (pricce_a/price_b)
+    return (price_a/price_b)
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    prices = {}
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        prices = {}
+       
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
+            prices[stock] = price
+        if 'ABC' in prices and 'DEF' in prices:
+            
+            print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
